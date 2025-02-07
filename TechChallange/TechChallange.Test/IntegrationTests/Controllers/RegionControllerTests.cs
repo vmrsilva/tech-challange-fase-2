1869672cd3afea9c28eff6ba6e0a033ec07c1d93
Valmir <50370623+vmrsilva@users.noreply.github.com>
2025-02-06 22:42:37 -0300

    test

--- TechChallange/TechChallange.Test/IntegrationTests/Controllers/RegionControllerTests.cs
+++ TechChallange/TechChallange.Test/IntegrationTests/Controllers/RegionControllerTests.cs
@@ -17,42 +17,52 @@ namespace TechChallange.Test.IntegrationTests.Controllers
         [Fact]
         public async Task Test()
         {
-            var client = _techChallangeApplicationFactory.CreateClient();
+            try
+            {
+                System.Console.WriteLine("Teste 1");
+                var client = _techChallangeApplicationFactory.CreateClient();
 
-            var response = await client.GetAsync("region/get-all?pageSize=10&page=1");
+                var response = await client.GetAsync("region/get-all?pageSize=10&page=1");
 
-            var resp = await response.Content.ReadAsStringAsync();
+                var resp = await response.Content.ReadAsStringAsync();
 
-            var result = JsonSerializer.Deserialize<BaseResponsePagedDto<IEnumerable<RegionResponseDto>>>(resp,
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                var result = JsonSerializer.Deserialize<BaseResponsePagedDto<IEnumerable<RegionResponseDto>>>(resp,
+                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.NotNull(result?.Data);
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(result?.Data);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                throw ex;
+            }
+         
         }
 
-        [Fact]
-        public async Task Teste2()
-        {
-            var client = _techChallangeApplicationFactory.CreateClient();
+        //[Fact]
+        //public async Task Teste2()
+        //{
+        //    var client = _techChallangeApplicationFactory.CreateClient();
 
 
-            var response = await client.GetAsync("region/get-all?pageSize=10&page=1");
+        //    var response = await client.GetAsync("region/get-all?pageSize=10&page=1");
 
-            var resp = await response.Content.ReadAsStringAsync();
+        //    var resp = await response.Content.ReadAsStringAsync();
 
-            var result = JsonSerializer.Deserialize<BaseResponsePagedDto<IEnumerable<RegionResponseDto>>>(resp,
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        //    var result = JsonSerializer.Deserialize<BaseResponsePagedDto<IEnumerable<RegionResponseDto>>>(resp,
+        //            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
 
-            var responseid = await client.GetAsync($"region/get-by-id/{result?.Data?.FirstOrDefault()?.Id}");
+        //    var responseid = await client.GetAsync($"region/get-by-id/{result?.Data?.FirstOrDefault()?.Id}");
 
-            var respId = await responseid.Content.ReadAsStringAsync();
+        //    var respId = await responseid.Content.ReadAsStringAsync();
 
-            var resultId = JsonSerializer.Deserialize<BaseResponseDto<RegionResponseDto>>(respId,
-            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        //    var resultId = JsonSerializer.Deserialize<BaseResponseDto<RegionResponseDto>>(respId,
+        //    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-            Assert.Equal(HttpStatusCode.OK, responseid.StatusCode);
-            Assert.Equal(result?.Data?.FirstOrDefault(), resultId?.Data);
-        }
+        //    Assert.Equal(HttpStatusCode.OK, responseid.StatusCode);
+        //    Assert.Equal(result?.Data?.FirstOrDefault(), resultId?.Data);
+        //}
     }
 }
